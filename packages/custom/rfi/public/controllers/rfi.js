effea8f77c5432dfbc13d1916b1a60bcfc4e96db
Naveen <naveen.srivastava@jktech.com>
2015-04-02 21:51:28 +0530

    Item Description is not working

--- packages/custom/rfi/public/controllers/rfi.js
+++ packages/custom/rfi/public/controllers/rfi.js
@@ -2,12 +2,13 @@
 
 angular.module('mean.rfi')
 
-.controller('createrfiController', ['$scope', 'Global', 'rfi',
+.controller('createrfiController',['$scope', 'Global', 'Rfi',
     function($scope, Global, Rfi) {
       $scope.global = Global;
-      alert('i am in controller 22');
-
+    //  alert('i am in controller 22');
+    console.log('Inside createrfiController');
       $scope.createNewRfi = function() {
+        console.log('Inside createrNewRFI 11111');
           var rfi = new Rfi({
           to: this.emailAddresses,
           mailingaddress: this.MailingAddress,
@@ -18,20 +19,42 @@ angular.module('mean.rfi')
           shipVia: this.shipVia,
           body: this.body,
           message: this.message,
-          approvedBy: this.approvedBy,
+          approvedBy: this.approvedBy
         });
-        alert(rfi);
+        console.log('Inside createrNewRFI');
         this.shipingAddress = rfi.mailingaddress;
       };
       
-    }
+    } 
 ]);
 
 angular.module('mean.rfi').controller('ItemsController', ['$scope','$rootScope', 
-  function($scope,$rootScope/*, isUserloggedin*/) {
-    $scope.items = [
-        {itemNumber:$rootScope.item.number, itemDetail:$rootScope.item.detail, itemQuantity: $rootScope.item.quantity, primary:true}];
-    $rootScope.contacts=$scope.contacts;
+  function($scope,$rootScope) {
+
+        $scope.items = 
+          [{itemNumber:$rootScope.item.number, itemDetail:$rootScope.item.detail, itemQuantity:$rootScope.item.quantity}];
+        
+        console.log('Hello');
+        $rootScope.items=$scope.items;
+  
+
+      
+    $scope.addItems = function() 
+    {
+      if($scope.item.number===undefined || $scope.item.detail===undefined)
+      {
+        $scope.contactError=true;
+        $scope.contactErrorMsg='Please enter a title and an email address for this contact';
+      }
+      else
+      {
+        $rootScope.items.push({itemNumber:$scope.item.number, itemDetail:$scope.item.detail, itemQuantity: $scope.item.quantity});
+      
+      $scope.item = '';
+      }
+    };
+  }
+]);
 
 
 /*$scope.deleteContact = function(contact)
@@ -60,25 +83,6 @@ angular.module('mean.rfi').controller('ItemsController', ['$scope','$rootScope',
     $scope.contacts.splice( index, 1 );   
   } 
 };*/
-
-$scope.addItem = function() 
-{
-  alert($scope.item.number+'   '+ $scope.item.detail);
-  if($scope.item.number===undefined || $scope.item.detail===undefined)
-  {
-    $scope.contactError=true;
-    $scope.contactErrorMsg='Please enter a title and an email address for this contact';
-  }
-  else
-  {
-    $rootScope.items.push({itemNumber:$scope.item.number, itemDetail:$scope.item.detail, itemQuantity: $scope.item.quantity});
-  
-  $scope.item = '';
-  }
-};
-}]);
-
-
 
 
 
